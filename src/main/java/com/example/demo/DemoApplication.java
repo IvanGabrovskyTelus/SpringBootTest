9de1558ce71171fe0b957c01d2166d351a4f7df2
Ivaylo Gabrovsky <ivaylo.gabrovsky@telus.com>
2022-05-19 10:24:34 -0400

    fixL Create Student REST API

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.student.Student;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@GetMapping("/api/v1/student")
+	public List<Student> hellow() {
+		return List.of(new Student(
+				1L, 
+				"Ivan Gabrovsky", 
+				"ivan.gabrovsky@telus.com", 
+				LocalDate.of(2002, Month.JUNE, 10), 
+				19));
 	}
 
 }
