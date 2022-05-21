f5fd3fc907572cd45fa526a4b0280dd0c307267f
Ivan Gabrovsky <x255748@corp.ads>
2022-05-20 22:38:03 -0400

    added an update request

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.student.Student;
@@ -54,5 +56,13 @@ public class DemoApplication {
 				.orElse(null);
 		return student;
 	}
+	
+	@PutMapping("/api/v1/student/{id}")
+	public Student updateStudent(@PathVariable String id, @RequestBody Student student ) {
+		System.out.println(student);
+		return student;
+	}
+	
+	
 
 }
