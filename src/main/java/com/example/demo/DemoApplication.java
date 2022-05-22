f9367727feeabae470ed39cf9440e1a939b34170
Ivaylo Gabrovsky <ivaylo.gabrovsky@telus.com>
2022-05-22 08:02:49 -0400

    add update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 // import org.springframework.context.annotation.Bean;
 
@@ -43,4 +46,15 @@ public class DemoApplication {
 		return repository.deleteById(Long.parseLong(id));
 	}
 	
+	@PutMapping("/api/v1/student/{id}")
+	public Student updateStudent(@PathVariable String id, @RequestBody Student student) {
+		// validate is id and student.id match
+		repository.update(student);
+		return student;
+	}
+	
+	@PostMapping("/api/v1/student")
+	public int createStudents(@RequestBody Student student) {
+		return repository.insert(student);
+	}
 }
--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -19,10 +19,10 @@ public interface StudentRepository {
 	@Delete("DELETE FROM students WHERE id = #{id}")
 	public int deleteById(long id);
 
-	@Insert("INSERT INTO students(id, name, passport) VALUES (#{id}, #{name}, #{passport})")
+	@Insert("INSERT INTO students(id, name, email, dob, age) VALUES (#{id}, #{name}, #{email}, #{age})")
 	public int insert(Student student);
 
-	@Update("Update students set name=#{name}, passport=#{passport} where id=#{id}")
+	@Update("Update students set name=#{name}, email=#{email}, age=#{age} where id=#{id}")
 	public int update(Student student);
 
 }
